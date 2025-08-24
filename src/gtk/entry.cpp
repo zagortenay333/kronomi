8d65af9aab344fd9b9a0c031a914e077fa0f963e
zagortenay333 <krionubis@gmail.com>
2025-08-24 22:48:50 +0200

    gg

--- src/gtk/entry.cpp
+++ src/gtk/entry.cpp
@@ -1,23 +1,29 @@
 #include <gtk/gtk.h>
 #include "base/core.h"
 
-#define fn(L) G_CALLBACK(+[]()L)
+#define fn(L) G_CALLBACK(+[](){L})
 
 #define APP_ID "org.gtk.Kronomi"
 
-static void activate (GtkApplication *app, Void *data) {
+static Void button_new (CString icon_name) {
+    Auto button = gtk_button_new();
+    Auto icon = gtk_picture_new_for_filename(icon_name);
+    gtk_button_set_child(GTK_BUTTON(button), icon);
+}
+
+static Void activate (GtkApplication *app, Void *data) {
     Auto window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Kronomi");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
 
-    Auto box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
-    gtk_widget_set_halign (box, GTK_ALIGN_CENTER);
-    gtk_widget_set_valign (box, GTK_ALIGN_CENTER);
+    Auto box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
 
-    gtk_window_set_child (GTK_WINDOW(window), box);
+    gtk_window_set_child(GTK_WINDOW(window), box);
 
     Auto button = gtk_button_new_with_label("Hello World");
-    g_signal_connect(button, "clicked", fn({ printf("hhhhhhhhhhhhh\n"); }), NULL);
+    g_signal_connect(button, "clicked", fn(printf("hhhhhhhhhhhhh\n");), NULL);
     gtk_box_append(GTK_BOX(box), button);
 
     gtk_window_present(GTK_WINDOW(window));
